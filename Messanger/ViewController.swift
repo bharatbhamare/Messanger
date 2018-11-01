55dde9643d5390d1b0d3c8e8d6701b862c6468da
kishor.barde <ujwala.atre@triarqhelath.com>
2018-11-01 18:56:30 +0530

    mac 1 change

--- Messanger/ViewController.swift
+++ Messanger/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("mac 2 changes")
+        print ("mac 1 change")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
