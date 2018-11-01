88ec1ebc0d21ea9dfb9fe9468b38ee1ec0f366f7
kishor.barde <ujwala.atre@triarqhelath.com>
2018-11-01 19:06:01 +0530

    mac 1 second change

--- Messanger/ViewController.swift
+++ Messanger/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("mac 2 changes")
         print ("mac 1 change")
+        print ("mac1 second change")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
