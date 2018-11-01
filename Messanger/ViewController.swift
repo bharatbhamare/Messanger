4b4bab616522cbf342ba0e695cf0bea569583d3a
bharat.bhamare <yogesh.khaire@triarqhealth.com>
2018-11-01 18:51:53 +0530

    mac2 changes

--- Messanger/ViewController.swift
+++ Messanger/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("mac 2 changes")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
